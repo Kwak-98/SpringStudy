65d4e60f31e9fda5cfb210baf946e23af9c6927e
Kwak-98 <64453121+Kwak-98@users.noreply.github.com>
2024-04-15 07:01:43 +0900

    [UPDATE]컴포넌트스캔 필터까지

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -10,8 +10,10 @@ import hello.core.discount.RateDiscountPolicy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
+@Component
 public class AppConfig {
 
     //@Bean memberService -> new MemoryMemberRepository()
